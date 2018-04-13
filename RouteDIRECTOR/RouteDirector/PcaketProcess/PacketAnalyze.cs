582a88bce4a3ac9276c58be92e63e6e95d37acaa
chenzhanghao <a1439458305@163.com>
2018-04-13 13:12:58 +0800

    3

--- RouteDIRECTOR/RouteDirector/PcaketProcess/PacketAnalyze.cs
+++ RouteDIRECTOR/RouteDirector/PcaketProcess/PacketAnalyze.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace RouteDirector.PacketAnalyze
+namespace RouteDirector.PacketProcess
 {
-	class PacketAnalyze
+	class PacketProcess
 	{
 
 	}
--- RouteDIRECTOR/RouteDirector/RouteDirectControl.cs
+++ RouteDIRECTOR/RouteDirector/RouteDirectControl.cs
@@ -5,7 +5,6 @@ using System.Net;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using RouteDirector.PacketAnalyze;
 using RouteDirector.PacketProcess;
 using RouteDirector.TcpSocket;
 
